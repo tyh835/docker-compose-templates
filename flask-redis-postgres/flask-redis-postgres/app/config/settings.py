d65856c471409844ca1d2e054f9d238046f4c5fb
Tony Han <itony9401@live.com>
2018-12-11 14:45:26 -0800

    added flask-redis-postgres

---
+++ flask-redis-postgres/flask-redis-postgres/app/config/settings.py
@@ -0,0 +1,7 @@
+SECRET_KEY = 'pickabettersecret'
+
+DEBUG = True
+TESTING = False
+
+SQLALCHEMY_DATABASE_URI = 'postgresql://flask:yourpassword@postgres:5432/flask'
+REDIS_URL = 'redis://redis:6379'
